079c1d6fdcf64b2cee7352540973c9a80706b21b
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-08-16 11:49:20 -0600

    Create Cadena.h

---
+++ Cadena.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#ifndef Cadena_H
+#define Cadena_H
+#endif
+//strcmpy compara 2 cadenas
+class Cadena {
+public:
+	Cadena(const char*);
+	Cadena(void);
+	friend Cadena operator +(const Cadena& ca1, const Cadena& ca2);
+	friend ostream& operator<<(ostream& out, const Cadena& c);
+	bool strcmp(const Cadena& c1);
+	Cadena substr(int pos,int length);
+
+private:
+	char* cadena;
+	int longitud;
+
+};
